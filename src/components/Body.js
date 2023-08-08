d2bb47caf6c72231fe5fe3e00f066c85beec69a5
shadevkumar <shadevsrk261@gmail.com>
2023-08-08 10:18:00 +0530

    Input Validation

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,44 +1,65 @@
-import Done from "./Done";
-import InProgress from "./InProgress";
-import Todo from "./Todo";
-import { useState, useRef, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { addTask,statusCheck } from "../features/tasks/taskSlice";
-import { addItemCard } from "../features/addItemCard/addItemCardSlice";
+import Done from './Done'
+import InProgress from './InProgress'
+import Todo from './Todo'
+import { useState, useRef, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { addTask, statusCheck } from '../features/tasks/taskSlice'
+import { addItemCard } from '../features/addItemCard/addItemCardSlice'
 
 const Body = () => {
-  const dispatch = useDispatch();
-  const showAddItemCard = useSelector((state) => state.addItemCard.flag);
+  const dispatch = useDispatch()
+  const showAddItemCard = useSelector((state) => state.addItemCard.flag)
 
-  const [taskTitle, setTaskTitle] = useState("");
-  const [taskDescription, setTaskDescription] = useState("");
-  const [status, setStatus] = useState("");
-  const containerRef = useRef();
+  const [taskTitle, setTaskTitle] = useState('')
+  const [error, setError] = useState('')
+  const [taskDescription, setTaskDescription] = useState('')
+  const [status, setStatus] = useState('')
+  const containerRef = useRef()
+
+  const taskItems = {
+    title: taskTitle,
+    description: taskDescription,
+    todo: true,
+    inProgress: false,
+    done: false,
+  }
 
   console.log(status)
 
   const handleClickOutside = (event) => {
     if (containerRef.current && !containerRef.current.contains(event.target)) {
-      dispatch(addItemCard(false));
+      dispatch(addItemCard(false))
     }
-  };
+  }
 
   useEffect(() => {
     if (showAddItemCard) {
-      document.addEventListener("mousedown", handleClickOutside);
+      document.addEventListener('mousedown', handleClickOutside)
     }
     return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  });
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  })
 
-  const taskItems = {
-    title: taskTitle,
-    description: taskDescription,
-    todo: false,
-    inProgress: true,
-    done: false,
-  };
+  const handleTitleInput = (event) => {
+    setTaskTitle(event.target.value)
+  }
+
+  const handleCreateTask = (event) => {
+    event.preventDefault()
+
+    if (taskTitle.length === 0) {
+      setError('Please enter a task title')
+      return
+    }
+    dispatch(addTask(taskItems))
+    dispatch(addItemCard(false))
+    setTaskTitle('')
+    setTaskDescription('')
+    dispatch(statusCheck(status))
+    setStatus('')
+    setError('')
+  }
 
   return (
     <>
@@ -50,8 +71,9 @@ const Body = () => {
           >
             <div>
               <input
+                type="text"
                 value={taskTitle}
-                onChange={(e) => setTaskTitle(e.target.value)}
+                onChange={handleTitleInput}
                 className="bg-transparent my-8 mx-8 mt-16 text-2xl font-bold focus:outline-none"
                 placeholder="Task Title"
               ></input>
@@ -65,12 +87,13 @@ const Body = () => {
 
             <div className="flex  items-center justify-between mb-10 ml-8">
               <div>
-         
                 <select
                   className="appearance-none text-center bg-[#3F3E51] px-2 py-1 text-sm rounded-xl focus:outline-none"
                   name="Status"
                   value={status}
-                  onChange={(e)=>{setStatus(e.target.value)}}
+                  onChange={(e) => {
+                    setStatus(e.target.value)
+                  }}
                 >
                   <option value="placeholder">Status</option>
                   <option value="todo">Todo</option>
@@ -100,14 +123,7 @@ const Body = () => {
               </div>
               <div className="items-center">
                 <button
-                  onClick={() => {
-                    dispatch(addTask(taskItems));
-                    dispatch(addItemCard(false));
-                    setTaskTitle("");
-                    setTaskDescription("");
-                    dispatch(statusCheck(status))
-                    setStatus('')
-                  }}
+                  onClick={handleCreateTask}
                   className="bg-[#575AC6] shadow-inner shadow-[#000000]/40 text-sm font-bold mx-4 mr-14 px-4 py-2  rounded-lg"
                 >
                   Create Task
@@ -117,9 +133,11 @@ const Body = () => {
           </div>
         ) : null}
 
+        <span className=''>{error && { error }}</span>
+
         <div className="mt-4 mr-2 px-4 py-2 w-[100%] flex justify-end ">
           <i className="fas fa-list mx-2 cursor-pointer"></i>
-          <i className="fas fa-th-large mx-2 cursor-pointer"></i>{" "}
+          <i className="fas fa-th-large mx-2 cursor-pointer"></i>{' '}
         </div>
         <div className="flex w-[100%] my-4 p-4 justify-between">
           <div className="mx-12 ">
@@ -134,7 +152,7 @@ const Body = () => {
         </div>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default Body;
+export default Body
