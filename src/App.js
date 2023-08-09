c54bd87e6936a19984009a086ce9d1838cb6f7d3
shadevkumar <shadevsrk261@gmail.com>
2023-08-09 13:21:17 +0530

    TaskDetails, removeItems reducer and some enhancements

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css'
 import Body from './components/Body'
 import AsideBar from './components/AsideBar'
+import { Route, Routes } from 'react-router-dom'
+import TaskDetails from './components/TaskDetails'
 
 function App() {
   return (
     <div className="App flex font-sans">
-      <AsideBar/>
-      <Body/>
+      <AsideBar />
+      <Routes>
+        <Route path="/" Component={Body} />
+        <Route path="/taskdetails/:taskid" Component={TaskDetails} />
+      </Routes>
     </div>
   )
 }
--- src/components/AsideBar.js
+++ src/components/AsideBar.js
@@ -1,26 +1,34 @@
-import { useDispatch } from "react-redux";
-import { addItemCard } from "../features/addItemCard/addItemCardSlice";
+import { useDispatch } from 'react-redux'
+import { addItemCard } from '../features/addItemCard/addItemCardSlice'
+import { Link } from 'react-router-dom'
+
 const AsideBar = () => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
   return (
     <nav className=" w-2/12  h-[100vh] flex flex-col justify-between items-center bg-[#1B1B27] text-slate-300  ">
       <div className="m-2 p-6  flex flex-col ">
         <span className="text-lg my-1 font-semibold cursor-pointer text-white">
           TaskFlow
         </span>
-        <button
-          onClick={() => {
-           dispatch(addItemCard(true))
-          }}
-          className="bg-[#3F3E51] px-2 py-1 my-4 rounded-lg cursor-pointer transition ease-in-out delay-50 hover:scale-105 hover:text-slate-50"
-        >
-          Add Task
-          <i className="fa-solid fa-plus text-sm ml-1"></i>
-        </button>
+        <Link to='/'>
+          <button
+            onClick={() => {
+              dispatch(addItemCard(true))
+            }}
+            className="bg-[#3F3E51] px-2 py-1 my-4 rounded-lg cursor-pointer transition ease-in-out delay-50 hover:scale-105 hover:text-slate-50"
+          >
+            Add Task
+            <i className="fa-solid fa-plus text-sm ml-1"></i>
+          </button>
+        </Link>
 
         <div className="my-2  border-b-2 border-[#3F3E51]"></div>
         <ul className="mt-3 ">
-          <li className="my-3 hover:text-slate-50 cursor-pointer">All Tasks</li>
+          <Link to="/">
+            <li className="my-3 hover:text-slate-50 cursor-pointer">
+              All Tasks
+            </li>
+          </Link>
           <li className="my-3 hover:text-slate-50 cursor-pointer">Active</li>
           <li className="my-3 hover:text-slate-50 cursor-pointer">Backlog</li>
         </ul>
@@ -30,7 +38,7 @@ const AsideBar = () => {
         <button>Sign in</button>
       </div>
     </nav>
-  );
-};
+  )
+}
 
-export default AsideBar;
+export default AsideBar
